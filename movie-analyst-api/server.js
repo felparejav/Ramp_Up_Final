aaffc33dd30c09d8286590b996f04893ded9c6fa
Andres Scarpetta <andres.scarpetta@endava.com>
2018-07-11 11:56:08 -0500

    Fixed api DB use and added seeds file

--- movie-analyst-api/server.js
+++ movie-analyst-api/server.js
@@ -1,94 +1,69 @@
 // Get our dependencies
-var express = require('express');
-var app = express();
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host     : process.env.DB_HOST || 'mysql-test.cxrpknmq0hfi.us-west-2.rds.amazonaws.com',
-  user     : process.env.DB_USER || 'applicationuser',
-  password : process.env.DB_PASS || 'applicationuser',
-  database : process.env.DB_NAME || 'movie_db'
-});
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const util = require('util')
 
-connection.connect();
-
-
-function getMovies(callback) {    
-        connection.query("SELECT * FROM movie_db.movies",
-            function (err, rows) {
-                callback(err, rows); 
-            }
-        );    
-}
-
-
-
-
-
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'applicationuser',
+  password: process.env.DB_PASS || 'applicationuser',
+  database: process.env.DB_NAME || 'movie_db'
+})
+pool.query = util.promisify(pool.query)
 
 // Implement the movies API endpoint
-app.get('/movies', function(req, res){
-
-    connection.query("select m.title, m.release, m.score, r.name as reviewer, p.name as publication from movie_db.movies m, movie_db.reviewers r, movie_db.publications p where r.publication=p.name and m.reviewer=r.name",function(err, rows){
-        res.json(rows);
-    });  
+app.get('/movies', async function (req, res) {
+  try {
+    const rows = await pool.query(
+      'select m.title, m.release_year, m.score, r.name as reviewer, p.name as publication from movies m,' +
+      'reviewers r, publications p where r.publication=p.name and m.reviewer=r.name'
+    )
+    res.json(rows)
+  } catch (err) {
+    console.error('API Error:', err)
+    res.staus(500).send({'msg': 'Internal server error'})
+  }
 })
 
-
-app.get('/', function(req, res, next) {   
-    //now you can call the get-driver, passing a callback function
-    getMovies(function (err, moviesResult){ 
-       //you might want to do something is err is not null...      
-       res.json(moviesResult);
-
-    });
-});
-
-// Implement the reviewers API endpoint
-app.get('/reviewers', function(req, res){
-  // Get a list of all of our reviewers
-  var authors = [
-    {name : 'Robert Smith', publication : 'The Daily Reviewer', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/angelcolberg/128.jpg'},
-    {name: 'Chris Harris', publication : 'International Movie Critic', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/bungiwan/128.jpg'},
-    {name: 'Janet Garcia', publication : 'MoviesNow', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/grrr_nl/128.jpg'},
-    {name: 'Andrew West', publication : 'MyNextReview', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/d00maz/128.jpg'},
-    {name: 'Mindy Lee', publication: 'Movies n\' Games', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/laurengray/128.jpg'},
-    {name: 'Martin Thomas', publication : 'TheOne', avatar : 'https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg'},
-    {name: 'Anthony Miller', publication : 'ComicBookHero.com', avatar : 'https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg'}
-  ];
-
-    connection.query("select r.name, r.publication, r.avatar from movie_db.reviewers r",function(err, rows){
-          res.json(rows);
-      });  
-  
+app.get('/reviewers', async function (req, res) {
+  try {
+    const rows = await pool.query('select r.name, r.publication, r.avatar from reviewers r')
+    res.json(rows)
+  } catch (err) {
+    console.error('API Error:', err)
+    res.staus(500).send({'msg': 'Internal server error'})
+  }
 })
 
-// Implement the publications API endpoint
-app.get('/publications', function(req, res){
-  // Get a list of publications
-  var publications = [
-    {name : 'The Daily Reviewer', avatar: 'glyphicon-eye-open'},
-    {name : 'International Movie Critic', avatar: 'glyphicon-fire'},
-    {name : 'MoviesNow', avatar: 'glyphicon-time'},
-    {name : 'MyNextReview', avatar: 'glyphicon-record'},
-    {name : 'Movies n\' Games', avatar: 'glyphicon-heart-empty'},
-    {name : 'TheOne', avatar : 'glyphicon-globe'},
-    {name : 'ComicBookHero.com', avatar : 'glyphicon-flash'}
-  ];
-
-    connection.query("select * from movie_db.publications",function(err, rows){
-          res.json(rows);
-      }); 
+app.get('/publications', async function (req, res) {
+  try {
+    const rows = await pool.query('select r.name, r.publication, r.avatar from reviewers r')
+    res.json(rows)
+  } catch (err) {
+    console.error('API Error:', err)
+    res.staus(500).send({'msg': 'Internal server error'})
+  }
 })
 
-// Implement the pending reviews API endpoint
-app.get('/pending', function(req, res){
-  // Get a list of pending movie reviews
-     connection.query("select m.title, m.release, m.score, r.name as reviewer, p.name as publication from movie_db.movies m, movie_db.reviewers r, movie_db.publications p where r.publication=p.name and m.reviewer=r.name and m.release=2018",function(err, rows){
-        res.json(rows);
-    });  
+app.get('/pending', async function (req, res) {
+  try {
+    const rows = await pool.query(
+      'select m.title, m.release, m.score, r.name as reviewer, p.name as publication' +
+      'from movie_db.movies m, movie_db.reviewers r, movie_db.publications p where' +
+      'r.publication=p.name and m.reviewer=r.name and m.release>=2017'
+    )
+    res.json(rows)
+  } catch (err) {
+    console.error('API Error:', err)
+    res.staus(500).send({'msg': 'Internal server error'})
+  }
 })
 
-console.log("server listening through port: "+process.env.PORT);
-// Launch our API Server and have it listen on port 3000.
-app.listen(process.env.PORT || 3000);
-module.exports = app;
+app.get('/', function (req, res) {
+  res.status(200).send({'service_status': 'Up'})
+})
+
+console.log('server listening through port: ' + process.env.PORT)
+app.listen(process.env.PORT || 3000)
+module.exports = app
--- movie-analyst-ui/server.js
+++ movie-analyst-ui/server.js
@@ -12,7 +12,7 @@ app.set('views', __dirname + '/public/views/');
 // This tells Express out of which directory to serve static assets like CSS and images
 app.use(express.static(__dirname + '/public'));
 
-
+let backend_url = process.env.BACKEND_URL || "localhost:3000"
 
 // The homepage route of our application does not interface with the MovieAnalyst API and is always accessible. We won’t use the getAccessToken middleware here. We’ll simply render the index.ejs view.
 app.get('/', function(req, res){
@@ -23,7 +23,7 @@ app.get('/', function(req, res){
 // Once the request is sent out, our API will validate that the access_token has the right scope to request the /movies resource and if it does, will return the movie data. We’ll take this movie data, and pass it alongside our movies.ejs template for rendering
 app.get('/movies', function(req, res){
   request
-    .get('http://localhost:3000/movies')
+    .get(`http://${backend_url}/movies`)
     .end(function(err, data) {
       if(data.status == 403){
         res.send(403, '403 Forbidden');
@@ -38,7 +38,7 @@ app.get('/movies', function(req, res){
 // The key difference on the authors route, is that for our client, we’re naming the route /authors, but our API endpoint is /reviewers. Our route on the client does not have to match the API endpoint route.
 app.get('/authors', function(req, res){
   request
-    .get('http://localhost:3000/reviewers')
+    .get(`http://${backend_url}/reviewers`)
     .set('Authorization', 'Bearer ' + req.access_token)
     .end(function(err, data) {
       if(data.status == 403){
@@ -52,7 +52,7 @@ app.get('/authors', function(req, res){
 
 app.get('/publications', function(req, res){
   request
-    .get('http://localhost:3000/publications')
+    .get(`http://${backend_url}/publications`)
     .end(function(err, data) {
       if(data.status == 403){
         res.send(403, '403 Forbidden');
@@ -66,7 +66,7 @@ app.get('/publications', function(req, res){
 // We’ve added the pending route, but calling this route from the MovieAnalyst Website will always result in a 403 Forbidden error as this client does not have the admin scope required to get the data.
 app.get('/pending', function(req, res){
   request
-    .get('http://localhost:3000/pending')
+    .get(`http://${backend_url}/pending`)
     .end(function(err, data) {
       if(data.status == 403){
         res.send(403, '403 Forbidden');
@@ -74,4 +74,4 @@ app.get('/pending', function(req, res){
     })
 })
 
-app.listen(3030);
+app.listen(process.env.PORT || 3030);
